f034c98c55d77159f5c6653a258d15793accc816
JacobAngeloSantos <santosjacobangelo@gmail.com>
2025-05-10 15:49:06 +0800

    Update JacobangelosantossHub.lua

--- JacobangelosantossHub.lua
+++ JacobangelosantossHub.lua
@@ -6,8 +6,9 @@ local Section = Tab:NewSection("Universal")
 Section:NewButton("Fly gui", "Fly gui", function()
     loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
 end)
-Section:NewButton("3rd person", "ButtonInfo", function()
-    local p=game:GetService("Players").LocalPlayer;local cam=workspace.CurrentCamera;cam.CameraType=Enum.CameraType.Scriptable;local uis=game:GetService("UserInputService");local rs=game:GetService("RunService");local d,h,minZ,maxZ,rS=10,5,5,20,0.3;local y=0;local rot=false;local last=Vector2.zero;uis.InputBegan:Connect(function(i)if(i.UserInputType==Enum.UserInputType.MouseButton2 or i.UserInputType==Enum.UserInputType.Touch)then rot=true;last=i.Position end end);uis.InputEnded:Connect(function(i)if(i.UserInputType==Enum.UserInputType.MouseButton2 or i.UserInputType==Enum.UserInputType.Touch)then rot=false end end);uis.InputChanged:Connect(function(i)if rot and(i.UserInputType==Enum.UserInputType.MouseMovement or i.UserInputType==Enum.UserInputType.Touch)then local delta=i.Position-last;y=y-delta.X*rS*0.01;last=i.Position elseif i.UserInputType==Enum.UserInputType.MouseWheel then d=math.clamp(d-i.Position.Z,minZ,maxZ)end end);rs.RenderStepped:Connect(function()if p.Character and p.Character:FindFirstChild("HumanoidRootPart")then local root=p.Character.HumanoidRootPart;local cf=CFrame.new(root.Position + (CFrame.Angles(0,y,0).LookVector * -d + Vector3.new(0,h,0)), root.Position + Vector3.new(0,h/2,0));cam.CFrame=cf end end)
-end)
-end)
+
+    
+    
+    Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
 end)
